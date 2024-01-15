8d43a9ea79fcb4f53b96b0272f734981e44e88aa
Titouan PODGORSKI <tipodgorsk>
2024-01-15 11:58:10 +0100

    Little problem --> On rentre deux fois dans l'interruption pour un seul appuie sur le boutton

--- Software/main.c
+++ Software/main.c
@@ -6,6 +6,7 @@
 #include <alt_types.h>
 #include "sys/alt_sys_init.h"
 
+
 void delay(){
 
     for(int j=0;j<100000;j++){};
@@ -14,29 +15,49 @@ void delay(){
 
 void Chenillard(){
 
-    int val_leds=0x01;
-    IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
+    static int val_leds=0x01;
+    //IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
 
     for(int i=0;i<8;i++){
         IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,val_leds);
         delay();
         val_leds = val_leds << 1;
     }
-    val_leds=0x01;
-
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x00);
 }
-
 
 static void irqhandler (void * context, alt_u32 id)
 {
-    //Chenillard();
-    IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
-    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
+    alt_printf("%x\n",IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE));
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0x01);
+    alt_printf("%x\n",IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE));
+    Chenillard();
+
+
+ //   IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
+  //  IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
 }
 
-int main(){
 
-    alt_irq_register( PIO_1_IRQ, NULL, (void*)irqhandler );
+static void init_button_pio()
+{
+
+    /* Enable all 4 button interrupts. */
+    IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE, 0x01);
+    /* Reset the edge capture register. */
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0x01);
+    /* Register the ISR. */
+
+    alt_irq_register(PIO_1_IRQ,NULL, (void*)irqhandler);
+}
+
+
+
+int main(){
+    alt_printf("Hello NIOS II\n");
+    // Initialise le processus d'interruption pour le PIO qui controle les bouttons
+    init_button_pio();
+
     while(1){};
     return 0;
 }
