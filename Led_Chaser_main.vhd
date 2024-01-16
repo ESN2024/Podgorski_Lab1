5db755a362d97fd5c45710b08cfdfd9e61b492b1
Titouan PODGORSKI <tipodgorsk>
2024-01-16 16:48:30 +0100

    Le button active et désactive un chanillard continu dont la vitesse est modulée par les 4 premiers switchs

--- Led_Chaser_main.vhd
+++ Led_Chaser_main.vhd
@@ -7,32 +7,36 @@ Entity Led_Chaser_main is port(
 
 	pio_bouton_poussoir : in  std_logic;
 	pio_out : out std_logic_vector(7 downto 0);
-	clk  : in std_logic;
+	clk : in std_logic;
+	pio_switchs : in  std_logic_vector(7 downto 0) := (others => 'X');
 	reset : in std_logic );
-	
+
 End entity Led_Chaser_main;
 
 
 architecture arch of Led_Chaser_main is
 	
-	component Led_chaser is
-	  port (
-			clk_clk                    : in  std_logic                    := 'X'; -- clk
-			pio_bouton_poussoir_export : in  std_logic                    := 'X'; -- export
-			pio_out_export             : out std_logic_vector(7 downto 0);        -- export
-			reset_reset_n              : in  std_logic                    := 'X'  -- reset_n
-	  );
-	end component Led_chaser;
 
-	begin
+    component Led_chaser is
+        port (
+            clk_clk                    : in  std_logic                    := 'X';             -- clk
+            pio_bouton_poussoir_export : in  std_logic                    := 'X';             -- export
+            pio_out_export             : out std_logic_vector(7 downto 0);                    -- export
+            reset_reset_n              : in  std_logic                    := 'X';             -- reset_n
+            pio_switchs_export         : in  std_logic_vector(7 downto 0) := (others => 'X')  -- export
+        );
+    end component Led_chaser;
+	 
+	 begin
 
-		u0 : component Led_chaser
-		  port map (
-				clk_clk                    => clk,                    --                 clk.clk
-				pio_bouton_poussoir_export => pio_bouton_poussoir, -- pio_bouton_poussoir.export
-				pio_out_export             => pio_out,             --             pio_out.export
-				reset_reset_n              => reset               --               reset.reset_n
-		  );
+    u0 : component Led_chaser
+        port map (
+            clk_clk                    => clk,                    --                 clk.clk
+            pio_bouton_poussoir_export => pio_bouton_poussoir, -- pio_bouton_poussoir.export
+            pio_out_export             => pio_out,             --             pio_out.export
+            reset_reset_n              => reset,              --               reset.reset_n
+            pio_switchs_export         => pio_switchs          --         pio_switchs.export
+        );
 
 		  
 end arch;
--- Software/main.c
+++ Software/main.c
@@ -6,60 +6,100 @@
 #include <alt_types.h>
 #include "sys/alt_sys_init.h"
 #include "unistd.h"
+#include <stdbool.h>
 
-void delay(){
-
-    for(int j=0;j<100000;j++){};
-}
-
+__uint32_t sleep_time=10000;
+bool flag_chenillard = 0;
 
 void Chenillard(){
 
     alt_printf("on entre dans la fonction chenillard \n");
-    int val_leds=0x01;
-    //IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
+    __uint8_t val_leds=0x01;
+    IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,val_leds);
 
-    for(int i=0;i<8;i++){
-        alt_printf("iter %x\n",i);
-        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,val_leds);
-        usleep(200000);
+    for(__uint8_t i=0;i<7;i++){
+        usleep(sleep_time);
         val_leds = val_leds << 1;
+        alt_printf("val leds : %x\n",val_leds);
+        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,val_leds);
     }
+    for(__uint8_t j=0;j<7;j++){
+        usleep(sleep_time);
+        val_leds = val_leds >> 1;
+        IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,val_leds);
+        alt_printf("val leds 2nd phase : %x\n",val_leds);
+    }
+    usleep(sleep_time);
     IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x00);
 }
 
-static void irqhandler (void * context, alt_u32 id)
+static void irqhandler_button (void * context, alt_u32 id)
 {
-    alt_printf("%x\n",IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE));
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0x01);
-    alt_printf("%x\n",IORD_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE));
-    Chenillard();
+    flag_chenillard=!flag_chenillard;
+}
 
 
- //   IOWR_ALTERA_AVALON_PIO_DIRECTION(PIO_0_BASE,0x01);
-  //  IOWR_ALTERA_AVALON_PIO_DATA(PIO_0_BASE,0x01);
+static void irqhandler_switchs (void * context, alt_u32 id)
+{
+    __uint8_t switchs_vals=0;
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE, 0x0f);
+
+    __uint8_t masque=0x0f;
+    switchs_vals=masque & IORD_ALTERA_AVALON_PIO_DATA(PIO_2_BASE);
+    alt_printf("switchsvals : %x\n",switchs_vals);
+    sleep_time = switchs_vals * 10000 + 10000;
+
+
 }
 
 
 static void init_button_pio()
 {
 
-    /* Enable all 4 button interrupts. */
+    /* Enable the button interrupts. */
     IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_1_BASE, 0x01);
     /* Reset the edge capture register. */
     IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_1_BASE, 0x01);
     /* Register the ISR. */
 
-    alt_irq_register(PIO_1_IRQ,NULL, (void*)irqhandler);
+    alt_irq_register(PIO_1_IRQ,NULL, (void*)irqhandler_button);
+}
+
+
+static void init_switchs_pio()
+{
+    __uint8_t switchs_vals=0;
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE, 0x0f);
+
+    __uint8_t masque=0x0f;
+    switchs_vals=masque & IORD_ALTERA_AVALON_PIO_DATA(PIO_2_BASE);
+    alt_printf("switchsvals : %x\n",switchs_vals);
+    sleep_time = switchs_vals * 10000 + 10000;
+
+    // Enable all 4 switchs interrupts.
+    IOWR_ALTERA_AVALON_PIO_IRQ_MASK(PIO_2_BASE, 0x0f);
+    // Reset the edge capture register.
+    IOWR_ALTERA_AVALON_PIO_EDGE_CAP(PIO_2_BASE, 0x0f);
+    // Register the ISR.
+
+    alt_irq_register(PIO_2_IRQ, NULL, (void*)irqhandler_switchs);
 }
 
 
 
 int main(){
     alt_printf("Hello NIOS II\n");
+
     // Initialise le processus d'interruption pour le PIO qui controle les bouttons
     init_button_pio();
+    init_switchs_pio();
 
-    while(1){};
+    while(1){
+
+        if(flag_chenillard==1){
+            Chenillard();
+        }
+    }
     return 0;
 }
